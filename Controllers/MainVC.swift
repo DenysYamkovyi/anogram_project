3bac36e104c30cbe92bdbaf9f0c4e1731156d739
Denys Yamkovyi <dyamkovyi@rightandabove.com>
2020-12-06 23:34:30 -0600

    init commit

---
+++ Controllers/MainVC.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  ParkChampTest
+//
+//  Created by macbook pro on 12/6/20.
+//
+
+import UIKit
+import Firebase
+
+class MainVC: UIViewController {
+    
+    @IBOutlet weak var wordsCount: UILabel!
+    
+    @IBOutlet weak var wordTextField: UITextField!
+    
+    var wordsArray: [String] = [String]()
+
+    var ref: DatabaseReference!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        wordsCount.text = "We'd like to find anograms"
+        wordTextField.returnKeyType = .done
+        wordTextField.delegate = self
+        
+        ref = Database.database().reference(withPath: "words")
+        getWords()
+    }
+    
+    func getWords() {
+        self.ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let wordsArr = snapshot.value as? Array<Any> {
+                for word in wordsArr {
+                    if word is String {
+                        self.wordsArray.append(word as! String)
+                        print("The values from the database: \(self.wordsArray)")
+                    }
+                }
+            }
+        })
+    }
+    
+    private func checkForAnogram() -> Int {
+        var anogramCount = 0
+        if let searchWord = wordTextField.text, searchWord != "" {
+            //taking each word in array from database
+            for eachWord in wordsArray {
+                if eachWord.count == searchWord.count {
+                    //get each character in "eachWord" to check
+                    var isAnogram = 0
+                    for character in Array(eachWord) {
+                        if searchWord.contains(character) {
+                            isAnogram = isAnogram + 1
+                        }
+                    }
+                    if isAnogram == searchWord.count {
+                        print("searchWord - \(searchWord) and anogram is - \(eachWord)")
+                        anogramCount = anogramCount + 1
+                    }
+                }
+            }
+            return anogramCount
+        }
+        return anogramCount
+    }
+}
+
+extension MainVC: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        wordTextField.resignFirstResponder()
+        let counter = checkForAnogram()
+        wordsCount.text = "We found \(counter) anogram(s)"
+        print("run the anogram logic")
+        return true
+    }
+}
